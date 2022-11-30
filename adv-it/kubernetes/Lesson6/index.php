673797388bfa1ffc8d15376aacbb3f6224f3106a
Yevgenii <yevgenii@test123.com>
2022-11-30 10:11:32 +0200

    add dockerfile,edit index.php file

--- adv-it/kubernetes/Lesson6/index.php
+++ adv-it/kubernetes/Lesson6/index.php
@@ -2,6 +2,6 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello from Kubernetes</h1>";
-echo "Server IP Address is: $ip_server"
-echo "Made by <font color=blue>Yevgenii"
+echo "Server IP Address is: $ip_server";
+echo "Made by <font color=blue>Yevgenii";
 ?>
